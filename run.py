b78c6abdff5a2f98ab20698d0affaa907ef818ce
Christin-nandy <achiengchristine14@gmail.com>
2020-06-30 16:17:34 +0300

    create run.py

---
+++ run.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3.6
+
+from user import User
+from credential import Credential
+
+def create_user(first_name,last_name,phone ,email):
+    '''
+    Function to create a new contact
+    '''
+    new_user = User(first_name,last_name,phone,email)
+    return new_user
+
+def create_credential(user_name,password):
+    '''
+    Function to create a new contact
+    '''
+    new_credential = Credential(user_name,password)
+    return new_credential
+    
+def save_users(user):
+    '''
+    Function to save user
+    '''
+    user.save_users()
+
+def save_credentials(credential):
+    '''
+    Function to save credential
+    '''
+    credential.save_credentials()            
--- user.py
+++ user.py
@@ -2,7 +2,7 @@ class User:
      """
     Class that generates new instances of users
     """
-       
+    pass     
 
     user_list = [] # Empty user list
 
